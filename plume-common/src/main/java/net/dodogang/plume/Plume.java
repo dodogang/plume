c8983a2aa6d0d40e9fa4bd5d154630ada3e3ecbe
Trikzon <trikzondev@gmail.com>
2021-02-06 16:08:49 -0500

    Use ash's ItemGroupBuilder

--- plume-common/src/main/java/net/dodogang/plume/Plume.java
+++ plume-common/src/main/java/net/dodogang/plume/Plume.java
@@ -1,5 +1,26 @@
 package net.dodogang.plume;
 
+import net.dodogang.ash.registry.ItemGroupBuilder;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
 public class Plume {
     public static final String MOD_ID = "plume";
+
+    public static final boolean runDevTests = false;
+
+    public static void init() {
+        initTest();
+    }
+
+    private static void initTest() {
+        if (!runDevTests) return;
+
+        ItemGroupBuilder.create(new Identifier(MOD_ID, "item_group"))
+                .icon(() -> new ItemStack(Items.ANDESITE))
+                .appendItems(itemStacks -> {
+                    itemStacks.add(new ItemStack(Items.ANDESITE_STAIRS));
+                }).build();
+    }
 }
