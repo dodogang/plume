8f9a0896a5e2113ea267839091f591dfe326aa75
Trikzon <trikzondev@gmail.com>
2021-02-12 21:09:50 -0500

    Add BatchRegister test

--- plume-common/src/main/java/net/dodogang/plume/Plume.java
+++ plume-common/src/main/java/net/dodogang/plume/Plume.java
@@ -1,9 +1,14 @@
 package net.dodogang.plume;
 
+import net.dodogang.ash.registry.BatchRegister;
 import net.dodogang.ash.registry.ItemGroupBuilder;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.dodogang.ash.registry.RegistrySupplier;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Plume {
     public static final String MOD_ID = "plume";
@@ -17,10 +22,26 @@ public class Plume {
     private static void initTest() {
         if (!runDevTests) return;
 
-        ItemGroupBuilder.create(new Identifier(MOD_ID, "item_group"))
+        ItemGroup itemGroup = ItemGroupBuilder.create(new Identifier(MOD_ID, "item_group"))
                 .icon(() -> new ItemStack(Items.ANDESITE))
-                .appendItems(itemStacks -> {
-                    itemStacks.add(new ItemStack(Items.ANDESITE_STAIRS));
-                }).build();
+                .build();
+
+        BatchRegister<Block> blockBatchRegister = BatchRegister.create(Registry.BLOCK_KEY, MOD_ID);
+        BatchRegister<Item> itemBatchRegister = BatchRegister.create(Registry.ITEM_KEY, MOD_ID);
+
+        final TestBlock block = new TestBlock(AbstractBlock.Settings.copy(Blocks.DIRT));
+        RegistrySupplier<TestBlock> testBlock = blockBatchRegister.add("test", () -> block);
+        RegistrySupplier<BlockItem> testBlockItem = itemBatchRegister.add(
+                "test", () -> new BlockItem(block, new Item.Settings().group(itemGroup))
+        );
+
+        blockBatchRegister.register();
+        itemBatchRegister.register();
+    }
+
+    private static class TestBlock extends Block {
+        public TestBlock(Settings settings) {
+            super(settings);
+        }
     }
 }
