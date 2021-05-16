586f320035c438ab4f66b663c6d428cba4c0fc6c
Trikzon <trikzon@protonmail.com>
2021-05-16 01:13:04 -0400

    Add reasons for previous mixins

--- common/src/main/java/net/dodogang/plume/mixin/PointOfInterestTypeAccessor.java
+++ common/src/main/java/net/dodogang/plume/mixin/PointOfInterestTypeAccessor.java
@@ -1,5 +1,6 @@
 package net.dodogang.plume.mixin;
 
+import net.dodogang.plume.registry.PointOfInterestTypeAppender;
 import net.minecraft.block.BlockState;
 import net.minecraft.world.poi.PointOfInterestType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,6 +9,10 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Necessary because these fields are private and are needed to add points of
+ * interest to villagers. See {@link PointOfInterestTypeAppender}
+ */
 @Mixin(PointOfInterestType.class)
 public interface PointOfInterestTypeAccessor {
     @Accessor("BLOCK_STATE_TO_POINT_OF_INTEREST_TYPE")
--- forge/src/main/java/net/dodogang/plume/ash/mixin/client/forge/ItemAccessor.java
+++ forge/src/main/java/net/dodogang/plume/ash/mixin/client/forge/ItemAccessor.java
@@ -9,6 +9,11 @@ import java.util.function.Supplier;
 
 @Mixin(Item.class)
 public interface ItemAccessor {
+    /**
+     * Necessary because setting the ISTER on forge is done through the block
+     * properties which is annoying for an abstraction mod.
+     * This might be replaced in the future with a better abstraction.
+     */
     // remap = false because this field is part of a forge patch and doesn't get remapped.
     @Accessor(value = "ister", remap = false)
     void setIster(Supplier<BuiltinModelItemRenderer> ister);
