cb8314df93b8a8d2e128855af4135296abed78ca
Daniel Orr <orrdaniel73@gmail.com>
2021-05-23 00:16:05 +0100

    fix cosmetics having damage tint

--- common/src/main/java/net/dodogang/plume/cosmetic/client/model/CosmeticModel.java
+++ common/src/main/java/net/dodogang/plume/cosmetic/client/model/CosmeticModel.java
@@ -2,8 +2,11 @@ package net.dodogang.plume.cosmetic.client.model;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.CompositeEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 
 @Environment(EnvType.CLIENT)
@@ -12,5 +15,10 @@ public abstract class CosmeticModel extends CompositeEntityModel<PlayerEntity> {
 
     public CosmeticModel(PlayerEntityRenderer renderer) {
         this.renderer = renderer;
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int i, int overlay, float f, float g, float h, float k) {
+        super.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, f, g, h, k);
     }
 }
